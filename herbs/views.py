7be803a5baaa3f3233f559159ecc394555a5c34b
George <gnjuki19@gmail.com>
2023-04-16 15:28:51 +0300

    Added permissions paramenters and rules

--- herbs/views.py
+++ herbs/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics
+# from rest_framework import generics, permissions  # new
 
 from .models import Herbs
+from .permissions import IsAuthorOrReadOnly  # new
 from .serializers import HerbsSerializer
 class HerbsList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Herbs.objects.all()
     serializer_class = HerbsSerializer
     
 class HerbsDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,) # new
+    # permission_classes = (permissions.IsAdminUser,) # new
     queryset = Herbs.objects.all()
     serializer_class = HerbsSerializer
 
